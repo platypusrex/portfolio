1927ed98e12b8e653217b008b797c6070418699d
platypusrex <frankcooke79@gmail.com>
2018-03-28 02:09:40 -0400

    still going

--- server/src/middleware/error-handler.middleware.js
+++ server/src/middleware/error-handler.middleware.js
@@ -1,13 +1,13 @@
-export async function errorHandler (ctx, next) {
-	try {
-		await next();
-	} catch (err) {
-		console.log('error:', err);
-		ctx.body = {
-			type: 'error',
-			message: err.message
-		};
-
-		ctx.status = err.status || 500;
-	}
-}
+// export async function errorHandler (ctx, next) {
+// 	try {
+// 		await next();
+// 	} catch (err) {
+// 		console.log('error:', err);
+// 		ctx.body = {
+// 			type: 'error',
+// 			message: err.message
+// 		};
+//
+// 		ctx.status = err.status || 500;
+// 	}
+// }
