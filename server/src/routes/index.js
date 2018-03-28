6d390900f0c1dafb0924856811e1550e77dbdbaa
platypusrex <frankcooke79@gmail.com>
2018-03-28 10:10:01 -0400

    Rework - imports and uses combineRouters middleware (this will allow me to keep separate routes files for each endpoint)

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,13 +1,4 @@
-import { routesLoader } from "../utils/routes-loader.util";
+import { combineRouters } from "../utils/routes-loader.util";
+import citiesRouter from './cities.routes';
 
-export function router (app) {
-	routesLoader(`${__dirname}`).then(files => {
-		files.forEach(route => {
-			app
-				.use(route.routes())
-				.use(route.allowedMethods({
-					throw: true
-				}));
-		});
-	});
-}
+export const router = combineRouters([citiesRouter]);
