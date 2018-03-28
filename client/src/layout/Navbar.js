d1cdbe0daee9f8154fb0a74211669816a355548b
platypusrex <frankcooke79@gmail.com>
2018-03-28 14:00:16 -0400

    Start - initial commit for Navbar component

---
+++ client/src/layout/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Navbar = () => (
+	<div style={{borderBottom: '1px solid #2a2a2a'}}>
+		<div style={{display: 'flex', flexDirection: 'column'}}>
+			<h1>Frank Cooke</h1>
+			<h5>Develops the Web</h5>
+		</div>
+	</div>
+);
