82039717f6a309dd44f00b88bf6c9dd712aed5fd
platypusrex <frankcooke79@gmail.com>
2018-03-28 14:04:39 -0400

    Rework - adds imports for fonts and fixes import for App component

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-montserrat'
+import 'typeface-cardo';
 import './index.css';
-import App from './App';
+import { App } from "./App";
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
