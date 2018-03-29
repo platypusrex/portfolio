b16193a64884a858f986d316c001bbe873ef0ed5
platypusrex <frankcooke79@gmail.com>
2018-03-28 23:26:02 -0400

    Rework - moving UIStateProvider and Router to index.js (now wrap the App component)

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import 'typeface-montserrat'
 import 'typeface-cardo';
 import './index.css';
 import { App } from "./App";
 import registerServiceWorker from './registerServiceWorker';
+import { UIStateProvider } from "./shared/containers/LayoutProvider";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<UIStateProvider>
+		<Router>
+			<App />
+		</Router>
+	</UIStateProvider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
