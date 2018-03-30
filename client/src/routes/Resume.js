d66092becc2a5a1072c8719f942882cf12670edf
platypusrex <frankcooke79@gmail.com>
2018-03-30 19:20:37 -0400

    Rework - adds PageHeader to Resume component

--- client/src/routes/Resume.js
+++ client/src/routes/Resume.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Container } from "../shared/components/Container";
+import { PageHeader } from "../shared/components/PageHeader";
 
 export const Resume = (props) => {
 	return (
 		<div className="resume">
-			I'm in the resume component
+			<PageHeader
+				title="Frank G. Cooke III"
+				subTitle={(
+					<React.Fragment>
+						frankcooke79@gmail.com&nbsp;&nbsp;|&nbsp;&nbsp;(843) 303-6284
+						<br/>Charleston, SC
+					</React.Fragment>
+				)}
+			/>
+
+			<Container>
+
+			</Container>
 		</div>
 	);
 };
