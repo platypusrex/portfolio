aac8f96c1f022b759c8d17053f3c83a634459dee
platypusrex <frankcooke79@gmail.com>
2018-03-30 19:20:14 -0400

    Rework - moves PageHeader outside of Container

--- client/src/routes/Contact.js
+++ client/src/routes/Contact.js
@@ -5,17 +5,19 @@ import { PageHeader } from "../shared/components/PageHeader";
 export const Contact = () => {
 	return (
 		<div className="contact">
+
+			<PageHeader
+				title="Contact"
+				subTitle={(
+					<React.Fragment>
+						Have a question or project to discuss?
+						<br/>Would love to hear from you!
+					</React.Fragment>
+				)}
+			/>
+
 			<Container>
 
-				<PageHeader
-					title="Contact"
-					subTitle={(
-						<React.Fragment>
-							Have a question or project to discuss?
-							<br/>Would love to hear from you!
-						</React.Fragment>
-					)}
-				/>
 			</Container>
 		</div>
 	);
