b25e091a2e1e4ee28d40577da8a56a3732dc8562
platypusrex <frankcooke79@gmail.com>
2018-03-30 15:07:00 -0400

    Rework - add prop types (className and style) to Container component

--- client/src/shared/components/Container.js
+++ client/src/shared/components/Container.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export const Container = (props) => {
+	const {className, style} = props;
+	const containerClass = `content-container ${className ? className : ''}`;
+
 	return (
-		<div className="content-container">
+		<div className={containerClass} style={style}>
 			{props.children}
 		</div>
 	);
+};
+
+Container.propTypes = {
+	className: PropTypes.string,
+	style: PropTypes.object
 };
