88d0d8f1d38016b87e9969905160c129226673cc
platypusrex <frankcooke79@gmail.com>
2018-03-30 09:15:33 -0400

    Rework - add MenuToggle component to Content component

--- client/src/layout/Content.js
+++ client/src/layout/Content.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { MenuToggle } from "../shared/components/MenuToggle";
 
 export const Content = (props) => {
 	const {children} = props;
 
 	return (
 		<div className="content">
+			<MenuToggle/>
+
 			{children}
 		</div>
 	)
