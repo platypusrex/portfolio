041b30a6415832f117c5a788bbc1b5d0f46c6bb6
platypusrex <frankcooke79@gmail.com>
2018-03-30 19:38:53 -0400

    Rework - move UIStateProvider inside Router to have access to withRouter HOC

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import 'typeface-montserrat'
 import 'typeface-cardo';
 import './index.css';
@@ -9,11 +9,13 @@ import registerServiceWorker from './registerServiceWorker';
 import { UIStateProvider } from "./shared/containers/LayoutProvider";
 
 ReactDOM.render(
-	<UIStateProvider>
-		<Router>
-			<App />
-		</Router>
-	</UIStateProvider>,
+	<Router>
+		<Switch>
+			<UIStateProvider>
+				<App />
+			</UIStateProvider>
+		</Switch>
+	</Router>,
 	document.getElementById('root')
 );
 registerServiceWorker();
