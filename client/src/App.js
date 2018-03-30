7b5e8cbd5eba652f564b4e9565e7318c9dbc2376
platypusrex <frankcooke79@gmail.com>
2018-03-30 15:09:17 -0400

    Rework - import and use Page component in application routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { compose } from "recompose";
 import { Route, Switch } from 'react-router-dom';
 import { Layout } from "./layout/Layout";
+import { Page } from "./shared/components/Page";
 import { About } from "./routes/About";
 import { Contact } from "./routes/Contact";
 import { Resume } from "./routes/Resume";
@@ -11,10 +12,10 @@ export const AppComponent = () => {
 	return (
 		<Layout>
 			<Switch>
-				<Route exact={true} path={'/'} component={About}/>
-				<Route path={'/contact'} component={Contact}/>
-				<Route path={'/resume'} component={Resume}/>
-				<Route component={NotFound}/>
+				<Route exact={true} path={'/'} component={Page(About)}/>
+				<Route path={'/contact'} component={Page(Contact)}/>
+				<Route path={'/resume'} component={Page(Resume)}/>
+				<Route component={Page(NotFound)}/>
 			</Switch>
 		</Layout>
 	);
