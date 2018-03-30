bdd722b2d55ae4f7fd28ff12c97b486737d8a1ca
platypusrex <frankcooke79@gmail.com>
2018-03-30 15:05:56 -0400

    Rework - add PageHeader to Contact component

--- client/src/routes/Contact.js
+++ client/src/routes/Contact.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Container } from "../shared/components/Container";
+import { PageHeader } from "../shared/components/PageHeader";
 
 export const Contact = () => {
 	return (
-		<div className="flex-container">
-			I'm in the contact route!
+		<div className="contact">
+			<Container>
+				<PageHeader
+					title="Contact"
+					subTitle={(
+						<React.Fragment>
+							Have a question or project to discuss?
+							<br/>Would love to hear from you!
+						</React.Fragment>
+					)}
+				/>
+			</Container>
 		</div>
 	);
 };
