6a4da1b780b00dec2749da86b2e3d42ee0209e98
platypusrex <frankcooke79@gmail.com>
2018-03-30 17:29:16 -0400

    Rework - adding custom hamburger menu to MenuToggle component

--- client/src/shared/components/MenuToggle.js
+++ client/src/shared/components/MenuToggle.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import { Icon } from "./Icon";
+import PropTypes from 'prop-types';
 
 export const MenuToggle = (props) => {
+	const {onClick, isOpen} = props;
+	const hamburgerClass = `hamburger ${isOpen ? 'open' : 'closed'}`;
+
 	return (
-		<a className="menu-toggle flex-container center-all">
-			<Icon className="ti-menu" size={35}/>
+		<a className="menu-toggle flex-container center-all" onClick={onClick}>
+			<div className={hamburgerClass}>
+				<span className="bar bar-1"/>
+				<span className="bar bar-2"/>
+				<span className="bar bar-3"/>
+			</div>
 		</a>
 	);
+};
+
+MenuToggle.propTypes = {
+	onClick: PropTypes.func,
+	isOpen: PropTypes.bool.isRequired
 };
