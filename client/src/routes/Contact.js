d8fee22724b2fd14bbea5dd76fbf7ecac6bd1c2e
platypusrex <frankcooke79@gmail.com>
2018-03-31 14:16:19 -0400

    Rework - contact page mockup continued

--- client/src/routes/Contact.js
+++ client/src/routes/Contact.js
@@ -4,10 +4,15 @@ import { PageHeader } from "../shared/components/PageHeader";
 import {Paragraph} from "../shared/components/Paragraph";
 import { SocialLink } from "../shared/components/SocialLink";
 import { Button } from "../shared/components/Button";
+import robot from '../assets/badass_robot.jpg';
 
 export const Contact = () => {
 	return (
 		<div className="contact">
+
+			<div className="contact__bg-image-wrapper">
+				<img className="contact__bg-image" src={robot} alt=""/>
+			</div>
 
 			<PageHeader
 				title="Contact"
@@ -20,15 +25,19 @@ export const Contact = () => {
 			/>
 
 			<Container>
-				<div className="grid">
+				<div className="contact__content-wrapper grid">
 					<div className="col-4_md-5_sm-12">
-						<Paragraph noMargin>frankcooke79@gmail.com</Paragraph>
-						<Paragraph noMargin>(843) 303-6284</Paragraph>
-						<Paragraph noMargin>Charleston, SC</Paragraph>
-
-						<div className="contact__social-link-wrapper flex-container center-row-vertical" style={{marginTop: '5px'}}>
-							<SocialLink icon="ti-linkedin" style={{marginRight: '12px'}}/>
-							<SocialLink icon="ti-github"/>
+						<div className="contact__contact-info-wrapper flex-container column">
+							<div className="contact__contact-info flex-container column">
+								<Paragraph className="contact__contact-info-title" noMargin>Contact info:</Paragraph>
+								<Paragraph noMargin>frankcooke79@gmail.com</Paragraph>
+								<Paragraph noMargin>(843) 303-6284</Paragraph>
+								<Paragraph noMargin>Charleston, SC</Paragraph>
+							</div>
+							<div className="contact__social-link-wrapper flex-container center-row-vertical" style={{marginTop: '5px'}}>
+								<SocialLink icon="ti-linkedin" style={{marginRight: '12px'}}/>
+								<SocialLink icon="ti-github"/>
+							</div>
 						</div>
 					</div>
 
