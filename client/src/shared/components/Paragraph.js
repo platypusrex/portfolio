43ddd3a8ad89f5771971c9e8d0a3959bf839d67b
platypusrex <frankcooke79@gmail.com>
2018-03-31 12:27:15 -0400

    Rework - adds className, size, noMargin props to the Paragraph component to make it more dynamic

--- client/src/shared/components/Paragraph.js
+++ client/src/shared/components/Paragraph.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { createClassName } from "../utils/createClassName";
+
+const textSize = {
+	xs: 'xs',
+	sm: 'sm',
+	md: 'md',
+	lg: 'lg'
+};
 
 export const Paragraph = (props) => {
+	const {prefixCls, className, noMargin} = props;
+	const sizeClass = getSizeClass(props);
+	const paragraphClass = `${prefixCls} ${className ? className : ''} ${sizeClass}`;
+
 	return (
-		<p className="paragraph">
-			{props.text}
+		<p className={paragraphClass} style={{marginBottom: noMargin ? 0 : null}}>
+			{props.children}
 		</p>
 	);
 };
 
+Paragraph.defaultProps = {
+	prefixCls: 'paragraph'
+};
+
+function getSizeClass (props) {
+	const {prefixCls, size} = props;
+
+	switch (size) {
+		case textSize.xs:
+			return createClassName(prefixCls, textSize.xs);
+		case textSize.sm:
+			return createClassName(prefixCls, textSize.sm);
+		case textSize.md:
+			return createClassName(prefixCls, textSize.md);
+		case textSize.lg:
+			return createClassName(prefixCls, textSize.lg);
+		default:
+			return createClassName(prefixCls, textSize.md)
+	}
+}
+
 Paragraph.propTypes = {
-	text: PropTypes.string.isRequired
+	className: PropTypes.string,
+	size: PropTypes.oneOf(Object.keys(textSize).map(key => textSize[key])),
+	noMargin: PropTypes.bool
 };
