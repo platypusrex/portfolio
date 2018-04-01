7011698abc2f23ef79c0399dd660e33fbe92756a
platypusrex <frankcooke79@gmail.com>
2018-04-01 13:16:10 -0400

    Add clientEmail, clientId, clientSecret, refreshToken, and AccessToken constants to config.js

--- server/src/config.js
+++ server/src/config.js
@@ -4,3 +4,10 @@ dotenv.load();
 
 export const secret = process.env.SECRET;
 export const port = process.env.PORT || 3001;
+
+// Nodemailer constants
+export const clientEmail = process.env.CLIENTEMAIL;
+export const clientId = process.env.CLIENTID;
+export const clientSecret = process.env.CLIENTSECRET;
+export const refreshToken = process.env.REFRESHTOKEN;
+export const accessToken = process.env.ACCESSTOKEN;
